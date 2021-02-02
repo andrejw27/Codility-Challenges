2e02275276f9f197240ab94f95b010dd19839930
andrejw27 <45046071+andrejw27@users.noreply.github.com>
2021-02-02 16:50:43 +0100

    Create MissingInteger.py

---
+++ Lesson 4 - Counting Elements/MissingInteger.py
@@ -0,0 +1,30 @@
+"""
+This is a demo task.
+
+Write a function:
+
+def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+"""
+
+# Missing Integers
+def solution(A):
+    res = set(range(1,len(A)+2)) - set(A)
+    res = sorted(res)
+    #print(res)
+    for i in res:
+        if i>0:
+            return i
+    return 1
